94c06e364d1463bd9e0bb8ce04df5891abc44f0a
Alexsabu <alexsabu053@gmail.com>
2025-07-23 10:09:52 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class odd 
+{
+public static void main(String [] args)
+{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the limit:");
+	int num =sc.nextInt();
+	for(int i=1;i<num;i++)
+	{
+		if(i%2==0)
+		{
+			continue;
+		}
+		else
+		{
+			System.out.println(i);
+		}
+	}
+}
+}
